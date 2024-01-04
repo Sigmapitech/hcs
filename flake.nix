8151f1f9f16d14ed66fa814a29c4aca610c490ae
Sigmanificient <edhyjox@gmail.com>
2024-01-04 10:03:56 +0100

    Port lambdananas to nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    lambdananas-src = {
+      url = "git+ssh://git@github.com/Epitech/lambdananas";
+      flake = false;
+    };
+  };
+
+  outputs = { self, lambdananas-src, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+
+        packages = {
+          lambdananas = pkgs.haskell.lib.buildStackProject {
+            name = "lambdananas";
+            src = lambdananas-src;
+            version = "2.0.0";
+
+            buildInputs = [ pkgs.haskell.compiler.integer-simple.ghc884 ];
+          };
+        };
+      });
+}
